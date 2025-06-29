9d15253d0a92335b2646c74287c53f37027eeba3
fasil <fasilsebsibe27@gmail.com>
2025-06-29 13:30:52 +0300

    add about page ui and funcitionlity

--- app/account/page.js
+++ app/account/page.js
@@ -1,5 +1,17 @@
-const Page = () => {
-  return <h1>it your account</h1>;
+import { auth } from "../_lib/auth";
+
+export const metadata = {
+  title: "Guest area",
 };
 
-export default Page;
+export default async function Page() {
+  const session = await auth();
+
+  const firstName = session.user.name.split(" ").at(0);
+
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {firstName}
+    </h2>
+  );
+}
